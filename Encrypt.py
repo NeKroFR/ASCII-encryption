2c30ca634d6374340b8d3d11915eb782040e17f9
Nekro <70316327+NeKroFR@users.noreply.github.com>
2021-12-02 15:07:25 +0100

    Update Encrypt.py

--- Encrypt.py
+++ Encrypt.py
@@ -1,7 +1,11 @@
 def encode (msg, key):
+    while (len(key) < len(msg)):
+        key += key
     encoded = ''
     for i in range (len(msg)):
         letter = ord(msg[i]) + ord(key[i])
+        while letter > 1114112:
+            letter -= 1114111
         encoded += chr(letter)
     return encoded
 
@@ -9,3 +13,4 @@ msg = str(input('message: '))
 key = str(input('key: '))
 encoded = encode(msg, key)
 print(encoded)
+file = open("encrypted.txt", "w").write(encoded)
